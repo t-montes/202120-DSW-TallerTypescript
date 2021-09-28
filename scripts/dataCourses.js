88aaaab6b13b2a0ccbeae91d269e1c33bb83c6fa
Tony Santiago Montes Buitrago <t.montes@uniandes.edu.co>
2021-09-28 00:25:19 -0500

    initial commit (no typescript added to html)

---
+++ scripts/dataCourses.js
@@ -0,0 +1,11 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Diseño y Análisis de Algoritmos", "Jorge Duitama", 3),
+    new Course("Lenguajes y Máquinas", "Nicolás Cardoso", 3),
+    new Course("Desarrollo de SW en Equipos", "José Bocanegra", 3),
+    new Course("Curso de Materiales", "Alba Ávila", 3),
+    new Course("Probabilidad y Estadística I", "Daniela Ortiz", 3),
+    new Course("Análisis de Decisiones de Inversión", "Sebastián Oyola", 3),
+    new Course("Física Moderna", "Benjamín Vannoppen", 3),
+    new Course("Cupitaller - Diseño y Programación O.O.", "Germán Romero", 0)
+];
